8d22d4dd9c490db1b4f8b54f7aa3bb75031865e2
Faizan Haq <fizoneechan@gmail.com>
2025-01-27 00:50:58 +0500

    make userid be optional

--- src/models/Payments.ts
+++ src/models/Payments.ts
@@ -5,7 +5,7 @@ const PaymentsSchema = new Schema(
     userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
     Subscription: {
       billingCycle: {
