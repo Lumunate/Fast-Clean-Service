968d0fe96f3fd3e091ad4ddd7aa8a081b1e8470e
Syed Muhammad Hassan Abbas Ali Naqvi <gaha87818@gmail.com>
2025-02-02 11:36:37 -0800

    Testimonials.jsx linked to en.json

--- src/components/Home/testimonials/Testimonials.jsx
+++ src/components/Home/testimonials/Testimonials.jsx
@@ -28,156 +28,158 @@ import HeadingLinesAnimation from "../HeadingLinesAnimation/HeadingLinesAnimatio
 import { useFeedback } from "../../../hooks/useFeedback";
 import { useTheme } from "../../../contexts/themeContext";
 import Image from "next/image";
+import {useTranslations} from "next-intl";
 
-const TESTIMONIALS = [
-  {
-    stars: 5,
-    name: "Emre",
-    details:
-      "Betrouwbaar en netjes.\nVandaag langsgeweest om de auto van binnen en buitenkant schoon te laten maken. Ik moet zeggen dat ik zeer tevreden ben met het resultaat!",
-    image: "https://swiperjs.com/demos/images/nature-3.jpg",
-    date: "December 13, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Richard Da Costa Abreu",
-    details:
-      "ENORM blij mee, een nette jonge man verscheen aan de deur en na een kort gesprekje van wensen etc is hij aan het werk gegaan. ik had de duurste pakket gekozen de combi de luxe paket buiten en binnen voor €189,95 . een hoop geld maar meer dan waard. hij is ruim 3 uur bezig geweest maar mijn auto is echt spik en span en alle kleine hoekjes en gaatjes, spleetjes tussen alle knopjes, vakjes ect etc zijn onder handen genomen hij was ook best vies moet ik toegeven. mijn auto leek wel nieuw zowel binnen als buiten net als hij de show room uit kwam. het begon later te regenen en je zag dat de regen druppels echt op de auto bleven liggen en er daarna afgleden nu 1 week later nog steeds het geval.  ik ben erg blij hij krijgt van mij een dikke 10. nogmaals bedankt.",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "Febuary 01, 2025",
-    socialIcons: [{ icon: "/Google.png", alt: "Google" }],
-  },
-
-  {
-    stars: 5,
-    name: "Klant",
-    details: "snel en superschoon!",
-    image: "https://swiperjs.com/demos/images/nature-1.jpg",
-    date: "April 25, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Igor Dotsenko",
-    details:
-      "I ordered exterior washing a few times already. Both times washerman arrived in time and did the work very well and professionally. Both times I was happy with the result and I will proceed using services provided by the company",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "June 06, 2023",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Radbout Platvoet",
-    details:
-      "Ik wilde mijn auto weer netjes hebben na 5 jaar gebruik. Ik heb er geen verstand van dus dan is het moeilijk een bedrijf te selecteren. Fast Clean werd me aangeraden dus dan doe je dat maar. Het was een volledig terechte verwijzing. Gewoon hele fijne mensen, geen haast, secuur en eerlijk.",
-    image: "https://swiperjs.com/demos/images/nature-3.jpg",
-    date: "November 15, 2024",
-    socialIcons: [{ icon: "/Google.png", alt: "Google" }],
-  },
-
-  {
-    stars: 5,
-    name: "AT",
-    details:
-      "Ze doen wat ze beloven. Prima\nAfspraak snel gemaakt. Geen lange wachttijd.\nKomen op afgesproken tijd. Vriendelijke werkers. \nResultaat is boven verwachting.",
-    image: "https://swiperjs.com/demos/images/nature-3.jpg",
-    date: "October 29, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Klant",
-    details:
-      "Top bedrijf\nTop bedrijf, lekker snel en uitstekend schoongemaakt. beveel ze bij iedereen aan!\nIk kom zeker bij ze terug voor me boot en caravan ;-)",
-    image: "https://swiperjs.com/demos/images/nature-1.jpg",
-    date: "September 24, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Harlememermeer Transport",
-    details: "Uitstekende service en het resultaat was super!",
-    image: "https://swiperjs.com/demos/images/nature-1.jpg",
-    date: "Febuary 01, 2024",
-    socialIcons: [{ icon: "/Google.png", alt: "Google" }],
-  },
-
-  {
-    stars: 5,
-    name: "C. R.",
-    details:
-      "Het was vrij simpel om de online…\nHet was vrij simpel om de online afspraak te maken. De service was inventief en volledig en dacht goed mee over de mogelijkheden. De auto was spik en span en weer als nieuw! En dat terwijl ik er geen vrij voor hoefde te nemen. Voorwaar een fantastische ervaring!",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "September 09, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Anthonie Lagerburg",
-    details:
-      "Top service en super netjes en correct\nAuto super netjes schoongemaakt van binnen en van buiten ziet er uit als nieuw ongebruikt",
-    image: "https://swiperjs.com/demos/images/nature-3.jpg",
-    date: "September 04, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Igor Dotsenko",
-    details:
-      "I ordered exterior washing a few times already. Both times washerman arrived in time and did the work very well and professionally. Both times I was happy with the result and I will proceed using services provided by the company",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "June 06, 2023",
-    socialIcons: [{ icon: "/Google.png", alt: "Google" }],
-  },
-
-  {
-    stars: 5,
-    name: "Nadine",
-    details: "Goed werk.\nAuto goed schoon! Had wat vlekken in de bekleding die er uit zijn.",
-    image: "https://swiperjs.com/demos/images/nature-1.jpg",
-    date: "September 04, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Klant",
-    details: "Goede service.\nGoede service, er wordt geurig gewerkt en het eindresultaat is super!",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "August 19, 2024",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-  {
-    stars: 5,
-    name: "Herman Bijkerk",
-    details:
-      "Wow. Wat een service! Auto als nieuw, zowel binnen als buiten. Interieur was vreselijk i.v.m. honden. Nu dus als nieuw. En erg vriendelijk!",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "April 04, 2023",
-    socialIcons: [{ icon: "/Google.png", alt: "Google" }],
-  },
-
-  {
-    stars: 5,
-    name: "Klant",
-    details:
-      "Top service en vriendelijke medewerker.\nTop service en vriendelijke medewerker. auto zag er weer als nieuw uit. Ik ga hier zeker vaker gebruik van maken. Bedankt!!!",
-    image: "https://swiperjs.com/demos/images/nature-2.jpg",
-    date: "August 16, 2023",
-    socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
-  },
-];
 
 export default function Testimonials() {
   const { theme } = useTheme();
   const sliderRef = useRef(null);
-
+  const t = useTranslations('home.customer_reviews_section');
   const [testimonials, setTestimonials] = useState(TESTIMONIALS);
   const [activeStep, setActiveStep] = useState(0);
   const [activeHeight, setActiveHeight] = useState("auto");
   const [isLargeScreen, setIsLargeScreen] = useState(false);
 
   const { feedbacks, fetchAll } = useFeedback();
+
+  const TESTIMONIALS = [
+    {
+      stars: 5,
+      name: "Emre",
+      details:
+          "Betrouwbaar en netjes.\nVandaag langsgeweest om de auto van binnen en buitenkant schoon te laten maken. Ik moet zeggen dat ik zeer tevreden ben met het resultaat!",
+      image: "https://swiperjs.com/demos/images/nature-3.jpg",
+      date: "December 13, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Richard Da Costa Abreu",
+      details:
+          "ENORM blij mee, een nette jonge man verscheen aan de deur en na een kort gesprekje van wensen etc is hij aan het werk gegaan. ik had de duurste pakket gekozen de combi de luxe paket buiten en binnen voor €189,95 . een hoop geld maar meer dan waard. hij is ruim 3 uur bezig geweest maar mijn auto is echt spik en span en alle kleine hoekjes en gaatjes, spleetjes tussen alle knopjes, vakjes ect etc zijn onder handen genomen hij was ook best vies moet ik toegeven. mijn auto leek wel nieuw zowel binnen als buiten net als hij de show room uit kwam. het begon later te regenen en je zag dat de regen druppels echt op de auto bleven liggen en er daarna afgleden nu 1 week later nog steeds het geval.  ik ben erg blij hij krijgt van mij een dikke 10. nogmaals bedankt.",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "Febuary 01, 2025",
+      socialIcons: [{ icon: "/Google.png", alt: "Google" }],
+    },
+
+    {
+      stars: 5,
+      name: "Klant",
+      details: "snel en superschoon!",
+      image: "https://swiperjs.com/demos/images/nature-1.jpg",
+      date: "April 25, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Igor Dotsenko",
+      details:
+          "I ordered exterior washing a few times already. Both times washerman arrived in time and did the work very well and professionally. Both times I was happy with the result and I will proceed using services provided by the company",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "June 06, 2023",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Radbout Platvoet",
+      details:
+          "Ik wilde mijn auto weer netjes hebben na 5 jaar gebruik. Ik heb er geen verstand van dus dan is het moeilijk een bedrijf te selecteren. Fast Clean werd me aangeraden dus dan doe je dat maar. Het was een volledig terechte verwijzing. Gewoon hele fijne mensen, geen haast, secuur en eerlijk.",
+      image: "https://swiperjs.com/demos/images/nature-3.jpg",
+      date: "November 15, 2024",
+      socialIcons: [{ icon: "/Google.png", alt: "Google" }],
+    },
+
+    {
+      stars: 5,
+      name: "AT",
+      details:
+          "Ze doen wat ze beloven. Prima\nAfspraak snel gemaakt. Geen lange wachttijd.\nKomen op afgesproken tijd. Vriendelijke werkers. \nResultaat is boven verwachting.",
+      image: "https://swiperjs.com/demos/images/nature-3.jpg",
+      date: "October 29, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Klant",
+      details:
+          "Top bedrijf\nTop bedrijf, lekker snel en uitstekend schoongemaakt. beveel ze bij iedereen aan!\nIk kom zeker bij ze terug voor me boot en caravan ;-)",
+      image: "https://swiperjs.com/demos/images/nature-1.jpg",
+      date: "September 24, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Harlememermeer Transport",
+      details: "Uitstekende service en het resultaat was super!",
+      image: "https://swiperjs.com/demos/images/nature-1.jpg",
+      date: "Febuary 01, 2024",
+      socialIcons: [{ icon: "/Google.png", alt: "Google" }],
+    },
+
+    {
+      stars: 5,
+      name: "C. R.",
+      details:
+          "Het was vrij simpel om de online…\nHet was vrij simpel om de online afspraak te maken. De service was inventief en volledig en dacht goed mee over de mogelijkheden. De auto was spik en span en weer als nieuw! En dat terwijl ik er geen vrij voor hoefde te nemen. Voorwaar een fantastische ervaring!",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "September 09, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Anthonie Lagerburg",
+      details:
+          "Top service en super netjes en correct\nAuto super netjes schoongemaakt van binnen en van buiten ziet er uit als nieuw ongebruikt",
+      image: "https://swiperjs.com/demos/images/nature-3.jpg",
+      date: "September 04, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Igor Dotsenko",
+      details:
+          "I ordered exterior washing a few times already. Both times washerman arrived in time and did the work very well and professionally. Both times I was happy with the result and I will proceed using services provided by the company",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "June 06, 2023",
+      socialIcons: [{ icon: "/Google.png", alt: "Google" }],
+    },
+
+    {
+      stars: 5,
+      name: "Nadine",
+      details: "Goed werk.\nAuto goed schoon! Had wat vlekken in de bekleding die er uit zijn.",
+      image: "https://swiperjs.com/demos/images/nature-1.jpg",
+      date: "September 04, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Klant",
+      details: "Goede service.\nGoede service, er wordt geurig gewerkt en het eindresultaat is super!",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "August 19, 2024",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+    {
+      stars: 5,
+      name: "Herman Bijkerk",
+      details:
+          "Wow. Wat een service! Auto als nieuw, zowel binnen als buiten. Interieur was vreselijk i.v.m. honden. Nu dus als nieuw. En erg vriendelijk!",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "April 04, 2023",
+      socialIcons: [{ icon: "/Google.png", alt: "Google" }],
+    },
+
+    {
+      stars: 5,
+      name: "Klant",
+      details:
+          "Top service en vriendelijke medewerker.\nTop service en vriendelijke medewerker. auto zag er weer als nieuw uit. Ik ga hier zeker vaker gebruik van maken. Bedankt!!!",
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+      date: "August 16, 2023",
+      socialIcons: [{ icon: "/Trustpilot.png", alt: "Trustpilot" }],
+    },
+  ];
 
   useEffect(() => {
     const updatedTestimonials = [...TESTIMONIALS];
@@ -250,7 +252,7 @@ export default function Testimonials() {
       }}
     >
       <ServicesOverviewWrapper>
-        <HeadingLinesAnimation sx={{ width: "50%", marginBottom: "7rem" }}>What Our Customers Say</HeadingLinesAnimation>
+        <HeadingLinesAnimation sx={{ width: "50%", marginBottom: "7rem" }}>{t("title")}</HeadingLinesAnimation>
         <Typography
           sx={{
             textAlign: "center",
@@ -266,7 +268,7 @@ export default function Testimonials() {
             },
           }}
         >
-          At Fast Clean Service we have already helped many satisfied customers.
+          {t("description")}
         </Typography>
         <Typography
           sx={{
@@ -283,7 +285,7 @@ export default function Testimonials() {
             },
           }}
         >
-          Leave Your Review and tell us how we helped you!
+          {t("description2")}
         </Typography>
         <Link href="/feedback" passHref>
           <Button
@@ -302,7 +304,7 @@ export default function Testimonials() {
               },
             }}
           >
-            Submit a Review
+            {t("button")}
           </Button>
         </Link>
       </ServicesOverviewWrapper>
