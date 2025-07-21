d6caed54603633f7e6c0736fb355180859c27df1
Hassan Abbas <gaha87818@gmail.com>
2025-07-21 15:14:51 +0500

    added all details of booking to dashboard

---
+++ src/components/Customer/BookingsTable.jsx
@@ -0,0 +1,7 @@
+// components/BookingsTable.tsx
+"use client";
+import React from "react";
+import BookingsPage from "../../app/[locale]/customer-portal/booking/page"; // wherever you extracted it
+export default function BookingsTable() {
+    return <BookingsPage />;
+}
